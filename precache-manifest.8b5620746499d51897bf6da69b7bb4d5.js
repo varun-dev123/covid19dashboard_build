59e83d32750c9ac6abf14e4ca0f1a760397fb1c6
varun-dev123 <varun.dev@actiknow.com>
2022-07-10 16:26:29 +0530

    first commit

---
+++ precache-manifest.8b5620746499d51897bf6da69b7bb4d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec3bbad38b58ebeac30772868da9a56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b766480f893f457e7a1",
+    "url": "/static/css/main.8d016bd7.chunk.css"
+  },
+  {
+    "revision": "943995f79d70251a618e",
+    "url": "/static/js/2.41950f92.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/static/js/2.41950f92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b766480f893f457e7a1",
+    "url": "/static/js/main.263bf9fa.chunk.js"
+  },
+  {
+    "revision": "d96ebe4897cd5e9524fd",
+    "url": "/static/js/runtime-main.f5bb9314.js"
+  }
+]);
